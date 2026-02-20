744dd5dcbcae2f895e1f731e1353e67bedcea14f
0xrafasec <rafaelgomes.web@gmail.com>
2026-02-20 17:21:44 -0300

    refactor: add missing exports for withAuth, buildUrl, and webhookVerification in utils index

--- packages/api/src/utils/index.ts
+++ packages/api/src/utils/index.ts
@@ -2,3 +2,6 @@ export * from "./httpClient";
 export * from "./errorHandler";
 export * from "./retryHandler";
 export * from "./defaultRetryConfig";
+export * from "./withAuth";
+export * from "./buildUrl";
+export * from "./webhookVerification";
