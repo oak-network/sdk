eb4fb5f3603c8688770179c837679fd068555d87
andrefelizardo <andreluis.felizardo@outlook.com>
2026-03-02 10:34:54 -0300

    chore: add Prettier configuration for code formatting

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 88,
+  tabWidth: 2,
+  arrowParens: "always",
+};
+
