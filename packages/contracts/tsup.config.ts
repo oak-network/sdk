e34c01f358f87d3579342ff68d3331029b047858
Mahabub Alahi <mahabub@ccprotocol.xyz>
2026-03-13 23:56:30 +0600

    chore: add metrics and constants modules to contract package
    - Updated package.json to include new metrics and constants modules, enhancing the modular structure of the contract package.
    - Modified tsup.config.ts to add entry points for the new metrics and constants modules, facilitating their inclusion in the build process.

--- packages/contracts/tsup.config.ts
+++ packages/contracts/tsup.config.ts
@@ -4,9 +4,12 @@ export default defineConfig({
   entry: {
     index: "src/index.ts",
     "utils/index": "src/utils/index.ts",
+    "constants/index": "src/constants/index.ts",
     "contracts/index": "src/contracts/index.ts",
     "client/index": "src/client/index.ts",
     "errors/index": "src/errors/index.ts",
+    "metrics/index": "src/metrics/index.ts",
+    "lib/privy/index": "src/lib/privy/index.ts",
   },
   format: ["esm"],
   dts: true,
